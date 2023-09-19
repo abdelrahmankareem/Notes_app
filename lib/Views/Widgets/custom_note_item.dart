7048ab70f402b020268796058c031d20830a9cff
abdelrahmankareem <abdoluka@gmail.com>
2023-09-19 04:51:42 +0300

    custom note item

---
+++ lib/Views/Widgets/custom_note_item.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class NoteItem extends StatelessWidget {
+  const NoteItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(
+        left: 16,
+        bottom: 24,
+        top: 24,
+      ),
+      decoration: BoxDecoration(
+        color: const Color(0xffffcc80),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.end, children: [
+        ListTile(
+          title: const Text(
+            "Welcome ",
+            style: TextStyle(color: Colors.black, fontSize: 26),
+          ),
+          subtitle: Padding(
+            padding: const EdgeInsets.only(top: 16),
+            child: Text("Write Your Notes With us ",
+                style: TextStyle(
+                    color: Colors.black.withOpacity(.5), fontSize: 18)),
+          ),
+          trailing: IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                FontAwesomeIcons.trash,
+                color: Colors.black,
+                size: 25,
+              )),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(
+            right: 16,
+            top: 24,
+          ),
+          child: Text("Sep 22/23",
+              style: TextStyle(color: Colors.black.withOpacity(.4))),
+        )
+      ]),
+    );
+  }
+}
