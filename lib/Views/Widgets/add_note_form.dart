0277fa47ff5f1a9248c4372b64ceec61dfd4169b
abdelrahmankareem <abdoluka@gmail.com>
2023-09-30 05:11:09 +0300

    create coloors list view part 1

--- lib/Views/Widgets/add_note_form.dart
+++ lib/Views/Widgets/add_note_form.dart
@@ -49,6 +49,7 @@ class _FormAddNoteState extends State<FormAddNote> {
             hint: "content",
             maxline: 5,
           ),
+          const ColorListView(),
           const SizedBox(
             height: 16,
           ),
@@ -82,6 +83,35 @@ class _FormAddNoteState extends State<FormAddNote> {
           )
         ],
       ),
+    );
+  }
+}
+
+class ColorItem extends StatelessWidget {
+  const ColorItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CircleAvatar(
+      backgroundColor: Colors.blue,
+      radius: 38,
+    );
+  }
+}
+
+class ColorListView extends StatelessWidget {
+  const ColorListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 38 * 2,
+      child: ListView.builder(
+          itemCount: 10,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return ColorItem();
+          }),
     );
   }
 }
